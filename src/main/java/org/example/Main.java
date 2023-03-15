0a5ba61d9197796c11859132bb7de216e1e4ca4f
Jakekick99 <jakekallarackal@gmail.com>
2023-03-15 21:02:29 +0530

    Testing github webhook again

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The world's first unscientific calculator. Now with many many devop tools behind it");
+        System.out.println("The world's first unscientific calculator. Now with many many DevOp tools behind it");
     }
 }
