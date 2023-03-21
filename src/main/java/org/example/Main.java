362f6f4172365ab2e9dc1d76da7df55cd579be48
Jakekick99 <jakekallarackal@gmail.com>
2023-03-21 14:51:24 +0530

    commiting some changes for screenshots

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("The world's first unscientific calculator. Now with many many DevOp tools behind it");
+        //some changes
     }
 }
